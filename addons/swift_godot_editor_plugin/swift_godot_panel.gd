0cb3020c0ff47e32f99c7db5ef6c48d8786d6108
Elijah Semyonov <elijah.semenov@gmail.com>
2025-04-23 10:33:06 +0200

    Refine previous implementation

--- addons/swift_godot_editor_plugin/swift_godot_panel.gd
+++ addons/swift_godot_editor_plugin/swift_godot_panel.gd
@@ -6,6 +6,7 @@ const MARGIN = 16
 @onready var recompile_button := Button.new()
 @onready var log_label := Label.new()
 @onready var log := RichTextLabel.new()
+@onready var status := Label.new()
 
 func _ready() -> void:
 	size_flags_vertical = Control.SIZE_EXPAND_FILL
@@ -20,6 +21,9 @@ func _ready() -> void:
 	recompile_button.size_flags_horizontal = Control.SIZE_SHRINK_BEGIN
 	add_child(recompile_button)
 	recompile_button.pressed.connect(recompile_swift)
+		
+	status.text = "Awaiting command"
+	add_child(status)
 	
 	log_label.text = "Log:"
 	add_child(log_label)
@@ -30,14 +34,15 @@ func _ready() -> void:
 	add_child(log)
 
 func append_log(string: String) -> void:
-	log.add_text(string)
+	log.add_text(string + "\n")
 
 func recompile_swift() -> void:
 	recompile_button.disabled = true
 	log.clear()
+	status.text = "Preparing..."
 	if OS.is_sandboxed():
 		recompile_button.disabled = false
-		printerr("Sandboxed :()")
+		append_log("Sandboxed :()")
 		return
 	var path = ProjectSettings.globalize_path("res://swift_godot_game")
 	var target_dir = ProjectSettings.globalize_path("res://addons/swift_godot_extension/bin")
@@ -46,8 +51,9 @@ func recompile_swift() -> void:
 		if err != OK:
 			append_log("Error creating directory '" + target_dir + "'")
 			return
-	
+		
 	var gdignore_path: String = target_dir.path_join(".gdignore")
+	append_log("Writing %s" % gdignore_path)
 	var file = FileAccess.open(gdignore_path, FileAccess.WRITE)
 	if file == null:
 		var open_error = FileAccess.get_open_error()
@@ -56,28 +62,30 @@ func recompile_swift() -> void:
 	else:
 		file.close()
 	
+	append_log("Building into %s" % target_dir)
 	var build_args = ["build", "--package-path", path]	
 	var output = []
-	var dictionary = OS.execute_with_pipe(
+	var pid := OS.create_process(
 		"swift", [
 			"build", "--package-path", path, "--build-path", target_dir
 		], false)
-	if dictionary.is_empty():
-		recompile_button.disabled = false
+	if pid == -1:
+		status.text = "Awaiting command"
+		log.append_text("Couldn't execute `swift build` command")
 		return
-	var stdio: FileAccess = dictionary["stdio"]
-	var pid: int = dictionary["pid"]
-	var progress_markers = ["/", "-", "\\", "|"]
-	var i = 0	
+	var progress_markers = ["˥", "˦", "˧", "˨", "˩", "˨", "˧", "˦"]
+	var i = 0
+	var start_time = Time.get_ticks_msec()	
+	status.text = "Running `swift build`"
 	while OS.is_process_running(pid):
 		await get_tree().create_timer(0.1).timeout
-		log.text = "Compiling " + progress_markers[i]
-		i = (i + 1) % progress_markers.size()	
-	append_log(stdio.get_as_text())
-	append_log("Relaunching in 3 seconds")
+		var time_passed = (Time.get_ticks_msec() - start_time) / 1000
+		status.text = "Compiling %s %s s " % [progress_markers[i], time_passed]
+		i = (i + 1) % progress_markers.size()
+	status.text = "Done"
+	append_log("Restarting editor in 3 seconds")
 	await get_tree().create_timer(3.0).timeout
 	var project_path = ProjectSettings.globalize_path("res://")
 	var arguments = ["--path", project_path, "--editor"]
-	var editor_exec_path = OS.get_executable_path()	
-	OS.execute(editor_exec_path, arguments)
-	get_tree().call_deferred("quit", 0)
+	var editor_exec_path = OS.get_executable_path()
+	EditorInterface.restart_editor(true)
